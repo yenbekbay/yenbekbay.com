cacd045257fc2dc4977e670463363eda96231527
Ayan Yenbekbay <ayan.yenb@gmail.com>
2024-07-16 13:34:42 +0500

    Update text

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,37 +7,29 @@ export default function Index() {
       <Article>
         <RevealGroupItem asChild>
           <p>
-            Ayan Yenbekbay is a software engineer based in Almaty, Kazakhstan.
+            Ayan Yenbekbay
           </p>
         </RevealGroupItem>
 
         <RevealGroupItem asChild>
           <p>
-            He specializes in digital products across the web and mobile
-            platforms, striving for beautiful execution and great user
-            experience. Through his practice,{' '}
-            <a href="https://utility-first.co" target="_blank">
-              Utility First
-            </a>
-            , he collaborates with a passionate net of talent, delivering
-            complete solutions for clients around the world.
+          Design-adjacent software engineer. Based in Almaty, Kazakhstan.
           </p>
         </RevealGroupItem>
 
         <RevealGroupItem asChild>
           <p>
-            <a href="mailto:ayan.yenb@gmail.com" target="_blank">
-              Email
-            </a>
-            <br />
             <a href="https://instagram.com/yenbekbay" target="_blank">
               Instagram
             </a>
             <br />
-            <a href="https://twitter.com/yenbekbay" target="_blank">
-              Twitter
+            <a href="mailto:ayan.yenb@gmail.com" target="_blank">
+              Email
             </a>
             <br />
+            <a href="https://t.me/yenbekbay" target="_blank">
+              Telegram
+            </a>
           </p>
         </RevealGroupItem>
       </Article>
