7792e48a707c09e2579ee41b339b757a458677f0
Ayan Yenbekbay <ayan.yenb@gmail.com>
2025-09-12 12:26:00 +0500

    Update copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,22 +11,24 @@ export default function Index() {
 
         <RevealGroupItem asChild>
           <p>
-            Software engineer and communication designer in Almaty, Kazakhstan.
+            Designer with a software engineering background.
+            <br />
+            Based in Almaty,&nbsp;Kazakhstan.
           </p>
         </RevealGroupItem>
 
         <RevealGroupItem asChild>
           <p>
-            <a href="https://instagram.com/yenbekbay" target="_blank">
-              Instagram
-            </a>
-            <br />
-            <a href="mailto:ayan.yenb@gmail.com" target="_blank">
+            <a href="mailto:ayan@utility-first.co" target="_blank">
               Email
             </a>
             <br />
             <a href="https://t.me/yenbekbay" target="_blank">
               Telegram
+            </a>
+            <br />
+            <a href="https://instagram.com/yenbekbay" target="_blank">
+              Instagram
             </a>
           </p>
         </RevealGroupItem>
